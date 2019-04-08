1d3cddcb5870c7b413dba0233e3cefe9967eff20
Philipp Moritz <pcmoritz@gmail.com>
2019-04-07 22:09:03 -0700

    update

--- webserverextension.py
+++ webserverextension.py
@@ -4,4 +4,4 @@ from subprocess import Popen
 def load_jupyter_server_extension(nbapp):
     # """serve the bokeh-app directory with bokeh server"""
     # Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
-    Popen(["pwd"])
+    Popen(["npm", "run", "dev"])
