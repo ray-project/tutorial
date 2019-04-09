1d5a357491f37f3a4bb1335c46b217ba0a5301f7
Philipp Moritz <pcmoritz@gmail.com>
2019-04-09 11:43:51 -0700

    add qreader server

--- tutorialextension.py
+++ tutorialextension.py
@@ -2,5 +2,8 @@ import os
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(cwd, "qreader", "dist"))
+    Popen(["python", "-m", "http.server", "9000"])
     os.chdir("/tmp")
     Popen(["python", "-m", "http.server"])
