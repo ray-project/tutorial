9179029d8a4edcc4780d0783f3597b38562a516a
Philipp Moritz <pcmoritz@gmail.com>
2019-04-10 16:26:05 -0700

    use shell

--- tutorialextension.py
+++ tutorialextension.py
@@ -4,7 +4,7 @@ from subprocess import Popen
 def load_jupyter_server_extension(nbapp):
     cwd = os.getcwd()
     os.chdir(os.path.join(cwd, "qreader", "dist"))
-    Popen(["python", "../../utilities/patch.py", "static/js/app.*.js"], shell=True)
+    Popen("python ../../utilities/patch.py static/js/app.*.js", shell=True)
 
     Popen(["python", "-m", "http.server", "9000"])
     os.chdir("/tmp")
