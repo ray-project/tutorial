1dccf82c03e7b6cbb980f851aef8614728f7e89d
Philipp Moritz <pcmoritz@gmail.com>
2019-04-15 23:23:03 -0700

    update

--- tutorialextension.py
+++ tutorialextension.py
@@ -6,6 +6,7 @@ def load_jupyter_server_extension(nbapp):
     # The following will replace "localhost" with the right binder url
     Popen("python utilities/patch.py examples/news_recommendation_serving.ipynb 9000", shell=True)
     Popen("python utilities/patch.py exercises/exercise01-Introduction.ipynb 8000", shell=True)
+    Popen("python utilities/patch.py exercises/exercise02-Task_Dependencies.ipynb 8000", shell=True)
     os.chdir(os.path.join(cwd, "qreader", "dist"))
     Popen("python ../../utilities/patch.py static/js/app.*.js 5000", shell=True)
 
