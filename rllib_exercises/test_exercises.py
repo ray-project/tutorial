5c409bbd973360c049cc0714f545323ae170efe4
Richard Liaw <rliaw@berkeley.edu>
2019-06-18 09:41:37 +0800

    flake

--- rllib_exercises/test_exercises.py
+++ rllib_exercises/test_exercises.py
@@ -9,10 +9,14 @@ def test_chain_env_spaces(chain_env_cls):
     assert test_env.action_space is not None, "Action Space not implemented!"
     assert test_env.observation_space is not None, "Observation Space not implemented!"
     assert not test_env.action_space.contains(2), "Action Space is only [0, 1]"
-    assert test_env.action_space.contains(1), "Action Space does not contain 1."
-    assert not test_env.observation_space.contains(6), "Observation Space is only [0..5]"
-    assert test_env.observation_space.contains(5), "Observation Space is only [0..5]"
+    assert test_env.action_space.contains(
+        1), "Action Space does not contain 1."
+    assert not test_env.observation_space.contains(
+        6), "Observation Space is only [0..5]"
+    assert test_env.observation_space.contains(
+        5), "Observation Space is only [0..5]"
     print("Success! You've setup the spaces correctly.")
+
 
 def test_chain_env_reward(chain_env_cls):
     test_env = chain_env_cls(dict(n=6))
@@ -26,12 +30,13 @@ def test_chain_env_reward(chain_env_cls):
     total_reward = 0
     for i in range(test_env.n - 1):
         total_reward += test_env.step(0)[1]
-    assert total_reward == 0, "Expected {} reward; got {}".format(0, total_reward)
+    assert total_reward == 0, "Expected {} reward; got {}".format(
+        0, total_reward)
     for i in range(3):
         assert test_env.step(0)[1] == test_env.large_reward
     assert test_env.step(1)[1] == test_env.small_reward
     print("Success! You've setup the rewards correctly.")
-    
+
 
 def test_chain_env_behavior(chain_env_cls):
     test_env = chain_env_cls(dict(n=6))
